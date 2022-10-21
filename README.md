f2cf8d4bd7bfbbbf7c21f265ddec2be2bfdc673c
subhransu sekhar mohanty <54876954+ssmohanty153@users.noreply.github.com>
2022-10-21 13:03:22 +0530

    Adding fSecond PortFolio

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portfolio
+
+### This is personal Portfolio website. Visit [Portfolio]( https://ssmohanty153-portfolio.netlify.app)
+
+![](/src/assets/subhransu_photo.jpg)
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { ThemeContext } from "../../ContextProvider/ThemeContext";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  const { newTheme } = React.useContext(ThemeContext);
+  return (
+    <footer
+      className={styles.footer}
+      id="contact"
+    >
+      <div className={styles.container}>
+        <h1 style={{ color: `${newTheme.title}` }}>Get in Touch</h1>
+        <p style={{ color: `${newTheme.para}` }} className={styles.paragraph}>
+          I'm actively looking for any new opportunities, in full-stack web
+          development.
+        </p>
+    
+          <p style={{ color: `#00a0a0`, lineHeight: ``,fontSize:`20px` }}>
+            {/* {" "} */}
+            Email: ssmohanty153@gmail.com
+          </p>
+
+		  <p style={{ color: `#00a0a0`,fontSize:`20px`}}>
+		  Mobile: +91-<span style={{ color: `#00a0a0`, fontSize:`20px`, textDecoration: `underline` }} >7978568916</span>{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+          </p>
+		  <p style={{ color: `#00a0a0`,fontSize:`20px` }}>
+            Location: Bangalore, India{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+          </p>
+		  
+        <div
+          style={{ color: `${newTheme.title}` }}
+          className={styles.contactOptions}
+        >
+          <a
+            href="tel:7978568916"
+            aria-label="GitHub"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fas fa-phone-alt" />
+          </a>
+          <a
+            href="mailto: ssmohanty153@gmail.com"
+            aria-label="email"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="far fa-envelope"></i>
+          </a>
+         
+          <a
+            href="https://github.com/ssmohanty153"
+            aria-label="GitHub"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fab fa-github" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/ssmohanty153"
+            aria-label="Linkedin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+        </div>
+      </div>
+      <div style={{ background: `${newTheme.line}` }} className={styles.line} />
+      <div style={{ color: `${newTheme.para}` }} className={styles.copyright}>
+        Designed and build by subhransu, 2022 All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
